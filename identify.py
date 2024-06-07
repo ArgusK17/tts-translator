1dcdc08fe6625c330fb18400a9b11247124e8f44
ArgusK17 <1800010727@pku.edu.cn>
2024-06-07 22:18:15 +0800

    mod

--- identify.py
+++ identify.py
@@ -51,7 +51,7 @@ def augmentSpecialWords(client, input, glossary):
 
 import numpy as np
 
-def get_embedding(text, model="text-embedding-3-small"):
+def get_embedding(client, text, model="text-embedding-3-small"):
     text = text.replace("\n", " ")
     embedding = client.embeddings.create(input=[text], model=model).data[0].embedding
     return np.array(embedding)
